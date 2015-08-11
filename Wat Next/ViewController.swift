fca777939dc49d37cb71b9d17b997b99007c1513
Sam <sambyoung@gmail.com>
2015-08-11 15:50:47 -0400

    Initial Commit

---
+++ Wat Next/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wat Next
+//
+//  Created by computer on 8/11/15.
+//  Copyright (c) 2015 computer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
