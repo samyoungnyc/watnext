2396ae2d28f9ba8e75730503b7954926e009cbd6
Sam <sambyoung@gmail.com>
2015-08-11 15:57:55 -0400

    Set up Parse

--- Wat Next/AppDelegate.swift
+++ Wat Next/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Parse
+import Bolts
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +18,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+            // [Optional] Power your app with Local Datastore. For more info, go to
+            // https://parse.com/docs/ios_guide#localdatastore/iOS
+            Parse.enableLocalDatastore()
+            
+            // Initialize Parse.
+            Parse.setApplicationId("orRVdhYXTKdCAGRLDnGunMS5AkkOtW8h05zuyze2",
+                clientKey: "aNtYN2fVmdfmInOcdQWrVGneCZzten5BCDQ3A2Qs")
+            
+            // [Optional] Track statistics around application opens.
+            PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
